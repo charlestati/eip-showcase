db40345076f30dc0dd8409402f7e162b52ee5fc8
charlestati <git@charlestati.com>
2017-07-19 00:12:40 +0200

    Fix deferred loading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
 import './vendor/bootstrap/collapse'
 
-window.addEventListener('load', () => {
-  $('.loader').fadeOut()
-})
+function run() {}
+
+// todo What if the event is never triggered? (blocked asset)
+if (document.readyState === 'complete') {
+  run()
+} else {
+  window.addEventListener('load', run)
+}
